0338d468aedacd004fe189b0911fc77ee1f77433
Igor-Tymchuk <dinamit.shuher@gmail.com>
2024-11-06 12:19:51 +0200

    add search Params

--- src/services/api.js
+++ src/services/api.js
@@ -3,14 +3,18 @@ import axios from "axios";
 const API_KEY = "EfRzRy1zLJ8fhBAdhkwX1z-xmM9Ij80iydVzG0zm98Y";
 axios.defaults.baseURL = "https://api.unsplash.com/";
 
-const params = {
-    headers: {
-        Authorization: `Client-ID ${API_KEY}`,
-    }
-}
-
 const fetchImg = async (topic, per_page = 10, page = 1) => {
-    const response = await axios(`search/photos?page=${page}&per_page=${per_page}&query=${topic}`, params);
+    const params = {
+        headers: {
+            Authorization: `Client-ID ${API_KEY}`,
+        },
+        params: {
+            per_page,
+            page,
+            query: topic,
+        }
+    }
+    const response = await axios(`search/photos?`, params);
     console.log(response.data.results);
     return response.data;
 };
